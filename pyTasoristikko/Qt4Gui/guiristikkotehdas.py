d0743b823e681627b4a49626344ce84001b73830
vleskinen <vleskinen@5ba45939-2f50-0410-a902-d532b781dbec>
2008-06-23 14:51:17 +0000

    Lisätään alustavasti ristikkotehdas käyttöliittymälle.

---
+++ pyTasoristikko/Qt4Gui/guiristikkotehdas.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# Tasoristikko: Tasristikkoja ratkaiseva ohjelma
+# Copyright (C) 2008 Ville Leskinen
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from pyTasoristikko.ristikkotehdas import Ristikkotehdas
+from guiristikko import *
+
+class GuiRistikkotehdas(Ristikkotehdas):
+    """Luo Qt4 käyttöliittymässä käytettävän ristikon."""
+    def __init__(self, scene, ristikko = None):
+        """Konstruktori.
+        @param scene: QRistikkoScene, johon tämä ristikko lisätään
+        @type scene:
+        L{QRistikkoScene<pyTasorististikko.Qt4Gui.ristikkonakyma.QRistikkoScene>}"""
+        Ritikkotehdas.__init__(self, ristikko)
+        self.scene = scene # QGraphicsScene johon ristikko kuuluu
+
+    def luoNivel(self, x, y, nimi):
+        nivel = QNivel(self.ristikko, self.scene, x, y)
+        return nivel
+
+    def luoSauva(self, nivel1, nivel2, nimi):
+        sauva = QSauva(self.ristikko, nivel1, nivel2)
+
+    def luoPistekuorma(self, nivel, kompX, kompY):
+        pistekuorma = QPistekuorma(nivel, kompX, kompY)
+
+    def luoTuki(self, nivel, suuntakulmat):
+        if len(suuntakulmat) == 1:
+            kulma = suuntakulmat[0] - 90.0
+            tuki = QRullatuki(nivel, kulma)
+        else:
+            # TODO: tarkistukset ja kulman laskenta
+            tuki = QNiveltuki(nivel)
